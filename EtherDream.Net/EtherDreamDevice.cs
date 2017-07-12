a62278884d417e71a4633323ee002b10f9114e7a
glopes <glopesdev@users.noreply.github.com>
2017-07-12 21:10:33 +0100

    Added documentation strings.

--- EtherDream.Net/EtherDreamDevice.cs
+++ EtherDream.Net/EtherDreamDevice.cs
@@ -7,16 +7,27 @@ using System.Threading.Tasks;
 
 namespace EtherDream.Net
 {
+    /// <summary>
+    /// Represents an Ether Dream device.
+    /// </summary>
     public class EtherDreamDevice : IDisposable
     {
         const int DeviceNameMaxLength = 256;
         int cardNumber;
 
-        private EtherDreamDevice(int number)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EtherDreamDevice"/> class
+        /// controlling the specified device number.
+        /// </summary>
+        /// <param name="number">The Ether Dream device number.</param>
+        public EtherDreamDevice(int number)
         {
             cardNumber = number;
         }
 
+        /// <summary>
+        /// Gets the device name.
+        /// </summary>
         public string Name
         {
             get
@@ -27,11 +38,17 @@ namespace EtherDream.Net
             }
         }
 
+        /// <summary>
+        /// Gets the ether dream device status.
+        /// </summary>
         public EtherDreamStatus Status
         {
             get { return NativeMethods.EtherDreamGetStatus(ref cardNumber); }
         }
 
+        /// <summary>
+        /// Opens a connection to the Ether Dream device.
+        /// </summary>
         public void Open()
         {
             if (!NativeMethods.EtherDreamOpenDevice(ref cardNumber))
@@ -40,6 +57,16 @@ namespace EtherDream.Net
             }
         }
 
+        /// <summary>
+        /// Writes a data frame to the Ether Dream device.
+        /// </summary>
+        /// <param name="data">The array of control points to send to the device.</param>
+        /// <param name="pointsPerSecond">The rate at which the device should draw the control points.</param>
+        /// <param name="repetitions">
+        /// How many times should the array be drawn by the device. A value of -1 specifies
+        /// infinite repetitions.
+        /// </param>
+        /// <returns><b>true</b> if the data frame was successfully transmitted; <b>false</b> otherwise.</returns>
         public bool WriteFrame(EtherDreamPoint[] data, int pointsPerSecond, int repetitions)
         {
             return NativeMethods.EtherDreamWriteFrame(
@@ -49,11 +76,18 @@ namespace EtherDream.Net
                 (ushort)repetitions);
         }
 
+        /// <summary>
+        /// Stops communication with the Ether Dream device.
+        /// </summary>
+        /// <returns><b>true</b> if the communication was successfully interrupted; <b>false</b> otherwise.</returns>
         public bool Stop()
         {
             return NativeMethods.EtherDreamStop(ref cardNumber);
         }
 
+        /// <summary>
+        /// Closes the connection to the Ether Dream device.
+        /// </summary>
         public void Close()
         {
             if (!NativeMethods.EtherDreamCloseDevice(ref cardNumber))
@@ -62,6 +96,14 @@ namespace EtherDream.Net
             }
         }
 
+        /// <summary>
+        /// Gets an array of the Ether Dream devices which are currently
+        /// connected into the computer.
+        /// </summary>
+        /// <returns>
+        /// An array of <see cref="EtherDreamDevice"/> instances representing the devices
+        /// which are currently connected into the computer.
+        /// </returns>
         public static EtherDreamDevice[] GetDevices()
         {
             var count = NativeMethods.EtherDreamGetCardNum();
